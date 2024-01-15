cb7f52f5d22c5bee20ef36e7f76cf0d1523a0a1c
Michael Masters <mastersm89@gmail.com>
2024-01-14 19:08:02 -0700

    Changed gallery to a slider viewer. Organized HTML better - improving readability     standardized naming scheme     grouped elements into divs in a more predictable manner Organized CSS better - improved readability Combined About Us and Quote into one row "row-2". Made row-2 responsive - stacking on screens under 400px. Fixed the footer to be displayed at the bottom of page and not overlap other elements. Site is looking good on mobile, needs work for desktop.

--- scripts/commonscripts.js
+++ scripts/commonscripts.js
@@ -6,23 +6,3 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-form.addEventListener('submit', function(event) {
-
-    event.preventDefault();
-
-    // get the data
-    const firstName = event.target.firstName.value
-    const lastName = event.target.lastName.value;
-    const phone = event.target.phone.value;
-    const email = event.target.email.value;
-    const address = event.target.address.value;
-    const project = event.target.project.value;
-
-    const subject = firstName + " " + lastName + ": Quote Request"
-    const body = encodeURIComponent(`Hello,\n\nYou have received a quote request from ${firstName} ${lastName}.\nHere are the details:\n\nEmail: ${email}\nPhone: ${phone}\nAddress: ${address}\nProject: ${project}\n\nThank you.`);
-
-    window.open('mailto:mastersm89@gmail.com?subject=subject&body=body');
-}) 
-
-
-   /*  const link = `mailto:${email}?subject=${subject}&body=${body}`; */
